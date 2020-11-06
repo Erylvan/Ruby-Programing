d1bca2b5d6e727b06585b781317c5e2932aedfc9
Eryl Van Kerckhove <eryl.vk@outlook.com>
2020-11-06 18:14:34 +0000

    Initial commits
    Providing some scripts created in the past for RPG maker VX ace and a disclaimer for those (and hopefully future) scripts that may be presented

---
+++ Paralax Mapping.rb
@@ -0,0 +1,156 @@
+# encoding: utf8
+# Script 51598340: Paralax Mapping
+# Paralax Mapping V.0.1
+# To use set up paralax as normal in map properties, however to apply a
+# overhead layer list the name of the file in the map notes with square
+# brackets (ie; [overhead name]).
+#
+# To use normal paralax scrolling, turn on paralax looping
+# To not use an overlay, do not reference a overhead within the map notes.
+#______________________________________________________________________________
+
+#==============================================================================
+# ** Cache
+#------------------------------------------------------------------------------
+#  This module loads graphics, creates bitmap objects, and retains them.
+# To speed up load times and conserve memory, this module holds the
+# created bitmap object in the internal hash, allowing the program to
+# return preexisting objects when the same bitmap is requested again.
+#==============================================================================
+module Cache
+  #--------------------------------------------------------------------------
+  # * Get Overhead Graphic
+  #--------------------------------------------------------------------------
+  def self.overhead(filename)
+    load_bitmap("Graphics/Overheads/", filename)
+  end
+end
+
+#==============================================================================
+# ** Game_Map
+#------------------------------------------------------------------------------
+#  This class handles maps. It includes scrolling and passage determination
+# functions. The instance of this class is referenced by $game_map.
+#==============================================================================
+class Game_Map
+  #--------------------------------------------------------------------------
+  # * Public Instance Variables
+  #--------------------------------------------------------------------------
+  attr_reader   :overhead_name            # overhead background filename
+  
+  #--------------------------------------------------------------------------
+  # * Setup
+  #--------------------------------------------------------------------------
+  alias evk_pm_gm_setup_42069 setup
+  def setup(map_id)
+    @map_id = map_id
+    @map = load_data(sprintf("Data/Map%03d.rvdata2", @map_id))
+    @tileset_id = @map.tileset_id
+    @display_x = 0
+    @display_y = 0
+    referesh_vehicles
+    setup_events
+    setup_scroll
+    setup_parallax
+    setup_overhead
+    setup_battleback
+    @need_refresh = false
+  end
+  #--------------------------------------------------------------------------
+  # * Overhead Setup
+  #--------------------------------------------------------------------------
+  def setup_overhead
+    if @map.note[/\[(.+)\]/]
+      @overhead_name = $1
+    else
+      @overhead_name = nil
+    end
+  end
+  #--------------------------------------------------------------------------
+  # * Calculate X Coordinate of Parallax Display Origin
+  #--------------------------------------------------------------------------
+  alias evk_pm_gm_paraOX_42069 parallax_ox
+  def parallax_ox(bitmap)
+    if @parallax_loop_x
+      @parallax_x * 16
+    else
+      w1 = [bitmap.width - Graphics.width, 0].max
+      w2 = [width * 32 - Graphics.width, 1].max
+      @parallax_x * 32 * w1 / w2
+    end
+  end
+  #--------------------------------------------------------------------------
+  # * Calculate Y Coordinate of Parallax Display Origin
+  #--------------------------------------------------------------------------
+  alias evk_pm_gm_paraOY_42069 parallax_oy
+  def parallax_oy(bitmap)
+    if @parallax_loop_y
+      @parallax_y * 16
+    else
+      h1 = [bitmap.height - Graphics.height, 0].max
+      h2 = [height * 32 - Graphics.height, 1].max
+      @parallax_y * 32 * h1 / h2
+    end
+  end
+end
+
+#==============================================================================
+# ** Spriteset_Map
+#------------------------------------------------------------------------------
+#  This class brings together map screen sprites, tilemaps, etc. It's used
+# within the Scene_Map class.
+#==============================================================================
+class Spriteset_Map
+  #--------------------------------------------------------------------------
+  # * Object Initialization
+  #--------------------------------------------------------------------------
+  alias evk_pm_sm_new_42069 initialize
+  def initialize
+    create_viewports
+    create_tilemap
+    create_parallax
+    create_characters
+    create_shadow
+    create_overhead
+    create_weather
+    create_pictures
+    create_timer
+    update
+  end
+  #--------------------------------------------------------------------------
+  # * Create Overhead
+  #--------------------------------------------------------------------------
+  def create_overhead
+    @overhead = Plane.new(@viewport1)
+    @overhead.z = 190
+  end
+  #--------------------------------------------------------------------------
+  # * Frame Update
+  #--------------------------------------------------------------------------
+  alias evk_pm_sm_update_42069 update
+  def update
+    update_tileset
+    update_tilemap
+    update_parallax
+    update_characters
+    update_shadow
+    update_overhead
+    update_weather
+    update_pictures
+    update_timer
+    update_viewports
+  end
+  #--------------------------------------------------------------------------
+  # * Update Overhead
+  #--------------------------------------------------------------------------
+  def update_overhead
+    if @overhead_name != $game_map.overhead_name
+      @overhead_name = $game_map.overhead_name
+      @overhead.bitmap.dispose if @overhead.bitmap
+      @overhead.bitmap = Cache.overhead(@overhead_name) if @overhead_name
+      Graphics.frame_reset
+    end
+    @overhead.ox = $game_map.parallax_ox(@parallax.bitmap)
+    @overhead.oy = $game_map.parallax_oy(@parallax.bitmap)
+  end
+end
---
+++ SaveRework.rb
@@ -0,0 +1,136 @@
+# encoding: utf8
+# Script 17801190: SaveRework
+# Save Rework V.0.1
+# Manages unlocked items, save updating and death counter
+
+#==============================================================================
+# ** ScriptCalls
+#------------------------------------------------------------------------------
+#  This module holds methods for managing simple functions
+#==============================================================================
+module ScriptCalls
+  #--------------------------------------------------------------------------
+  # * Logs log to be stored in array
+  #--------------------------------------------------------------------------
+  def self.unlock_log(id)
+    $game_logs[id] = 1
+    $game_map.need_refresh = true
+  end
+  #------------------------------------------------------------------------
+  # * Gets stored logs
+  #------------------------------------------------------------------------
+  def self.get_logs
+    $game_logs.keys
+  end
+  #------------------------------------------------------------------------
+  # * Gets stored logs
+  #------------------------------------------------------------------------
+  def self.get_death_counter
+    $game_death_counter
+  end
+  #------------------------------------------------------------------------
+  # * Handles game over
+  #------------------------------------------------------------------------
+  def self.game_over
+    $game_death_counter += 1
+    DataManager.update_save_contents
+    SceneManager.goto(Scene_Gameover)
+  end
+end
+
+#==============================================================================
+# ** DataManager
+#------------------------------------------------------------------------------
+#  This module manages the database and game objects. Almost all of the 
+# global variables used by the game are initialized by this module.
+#==============================================================================
+module DataManager
+  class << self
+    #------------------------------------------------------------------------
+    # * Create Game Objects
+    #------------------------------------------------------------------------
+    alias evk_SSaveRework_dm_cgo_42069  create_game_objects
+    def create_game_objects
+      evk_SSaveRework_dm_cgo_42069
+      $game_logs          = {}
+      $game_death_counter = 0
+    end
+    #------------------------------------------------------------------------
+    # * Set Up New Game
+    #------------------------------------------------------------------------
+    alias evk_SSaveRework_dm_sng_42069  setup_new_game
+    def setup_new_game
+      create_game_objects
+      $game_party.setup_starting_members
+      @last_savefile_index = -1
+      $game_map.setup($data_system.start_map_id)
+      $game_player.moveto($data_system.start_x, $data_system.start_y)
+      $game_player.refresh
+      Graphics.frame_count = 0
+    end
+    #------------------------------------------------------------------------
+    # * Create Save Contents
+    #------------------------------------------------------------------------
+    alias evk_SSaveRework_dm_msc_42069  make_save_contents
+    def make_save_contents
+      contents = {}
+      contents[:system]        = $game_system
+      contents[:timer]         = $game_timer
+      contents[:message]       = $game_message
+      contents[:switches]      = $game_switches
+      contents[:variables]     = $game_variables
+      contents[:self_switches] = $game_self_switches
+      contents[:actors]        = $game_actors
+      contents[:party]         = $game_party
+      contents[:troop]         = $game_troop
+      contents[:map]           = $game_map
+      contents[:player]        = $game_player
+      contents[:logs]          = $game_logs
+      contents[:deaths]        = $game_death_counter
+      contents
+    end
+    #------------------------------------------------------------------------
+    # * Extract Save Contents
+    #------------------------------------------------------------------------
+    alias evk_SSaveRework_dm_esc_42069  extract_save_contents
+    def extract_save_contents(contents)
+      $game_system        = contents[:system]
+      $game_timer         = contents[:timer]
+      $game_message       = contents[:message]
+      $game_switches      = contents[:switches]
+      $game_variables     = contents[:variables]
+      $game_self_switches = contents[:self_switches]
+      $game_actors        = contents[:actors]
+      $game_party         = contents[:party]
+      $game_troop         = contents[:troop]
+      $game_map           = contents[:map]
+      $game_player        = contents[:player]
+      $game_logs          = contents[:logs]
+      $game_death_counter = contents[:deaths]
+    end
+    #------------------------------------------------------------------------
+    # * Update Save Contents
+    #------------------------------------------------------------------------
+    def update_save_contents
+      if @last_savefile_index != -1
+        File.open(make_filename(@last_savefile_index), "rb") do |file|
+          Marshal.load(file)
+          evk_SSaveRework_dm_esc_42069(Marshal.load(file))
+          reload_map_if_updated
+        end
+        File.open(make_filename(@last_savefile_index), "wb") do |file|
+          $game_system.on_before_save
+          Marshal.dump(make_save_header, file)
+          Marshal.dump(make_save_contents, file)
+        end
+      end
+    end
+    #--------------------------------------------------------------------------
+    # * Get Index of File Most Recently Accessed
+    #--------------------------------------------------------------------------
+    alias evk_SSaveRework_dm_lsi_42069  last_savefile_index
+    def last_savefile_index
+      @last_savefile_index == -1 ? 0 : @last_savefile_index
+    end
+  end
+end
